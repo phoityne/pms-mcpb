d8fe6edaa7e6cfcd81312db8c989c6622fb248c8
phoityne.hs@gmail.com <phoityne.hs@gmail.com>
2025-11-16 15:20:44 +0900

    up.

--- create_mcpb.sh
+++ create_mcpb.sh
@@ -4,18 +4,16 @@ version=$1
 
 ls -la bin
 
-echo "Decompressing .xz binaries in bin/..."
-for file in bin/*.xz; do
+echo "Decompressing .xz binaries in ./"
+for file in *.xz; do
   [ -e "$file" ] || continue
   echo "Decompressing $file"
   xz -d -k "$file"
 done
 
-mv bin/pty-mcp-server-macos-aarch64 bin/pty-mcp-server
-mv bin/pty-mcp-server-windows-no-mingw.exe bin/pty-mcp-server.exe
+mv bin/pty-mcp-server-macos-aarch64-${version} bin/pty-mcp-server
+mv bin/pty-mcp-server-windows-no-mingw-${version}.exe bin/pty-mcp-server.exe
 
 mcpb pack . pty-mcp-server-${version}.mcpb
-
-ls -la download
 
 exit 0
